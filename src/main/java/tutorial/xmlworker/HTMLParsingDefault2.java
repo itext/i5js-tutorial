88f697545a4e375a36f43765497e4194b21e4d15
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2011-06-15 12:50:34 +0000

    Changing dependencies. Registering fonts in example.

--- src/main/java/tutorial/xmlworker/HTMLParsingDefault2.java
+++ src/main/java/tutorial/xmlworker/HTMLParsingDefault2.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 public class HTMLParsingDefault2 {
 
 	public static void main(String[] args) throws IOException, DocumentException {
+		FontFactory.registerDirectories();
 		Document document = new Document();
 		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("results/walden2.pdf"));
 		writer.setInitialLeading(12.5f);
