abd78d7de3894d9eec6fad6ddad369b156528748
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-08-02 14:44:14 +0000

    Extra examples involving signature appearances

--- src/main/java/tutorial/signatures/E07_CreateEmptyField.java
+++ src/main/java/tutorial/signatures/E07_CreateEmptyField.java
@@ -22,6 +22,8 @@ import com.itextpdf.text.pdf.ColumnText;
 import com.itextpdf.text.pdf.PdfAnnotation;
 import com.itextpdf.text.pdf.PdfAppearance;
 import com.itextpdf.text.pdf.PdfFormField;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.security.DigestAlgorithms;
 import com.itextpdf.text.pdf.security.MakeSignature;
@@ -33,20 +35,24 @@ public class E07_CreateEmptyField {
 	public static final String UNSIGNED = "results/signatures/hello_empty.pdf";
 	public static final String SIGNAME = "Signature1";
 	public static final String DEST = "results/signatures/field_signed.pdf";
+
+	public static final String SRC = "src/main/resources/signatures/hello.pdf";
+	public static final String UNSIGNED2 = "results/signatures/hello_empty2.pdf";
 	
     public void createPdf(String filename) throws IOException, DocumentException {
-    	// step 1
+    	// step 1: Create a Document
         Document document = new Document();
-        // step 2
+        // step 2: Create a PdfWriter
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filename));
-        // step 3
+        // step 3: Open the Document
         document.open();
-        // step 4
+        // step 4: Add content
         document.add(new Paragraph("Hello World!"));
         // create a signature form field
         PdfFormField field = PdfFormField.createSignature(writer);
         field.setFieldName(SIGNAME);
         // set the widget properties
+        field.setPage();
         field.setWidget(new Rectangle(72, 732, 144, 780), PdfAnnotation.HIGHLIGHT_INVERT);
         field.setFlags(PdfAnnotation.FLAGS_PRINT);
         // add it as an annotation
@@ -60,13 +66,29 @@ public class E07_CreateEmptyField {
         tp.setColorFill(BaseColor.BLUE);
         ColumnText.showTextAligned(tp, Element.ALIGN_CENTER, new Phrase("SIGN HERE"), 36, 24, 25);
         field.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, tp);
-        // step 5
+        // step 5: Close the Document
         document.close();
+    }
+    
+    public void addField(String src, String dest) throws IOException, DocumentException {
+    	PdfReader reader = new PdfReader(src);
+    	PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        // create a signature form field
+        PdfFormField field = PdfFormField.createSignature(stamper.getWriter());
+        field.setFieldName(SIGNAME);
+        // set the widget properties
+        field.setWidget(new Rectangle(72, 732, 144, 780), PdfAnnotation.HIGHLIGHT_OUTLINE);
+        field.setFlags(PdfAnnotation.FLAGS_PRINT);
+        // add the annotation
+        stamper.addAnnotation(field, 1);
+        // close the stamper
+    	stamper.close();
     }
     
     public static void main(String[] args) throws IOException, DocumentException, GeneralSecurityException {
     	E07_CreateEmptyField appCreate = new E07_CreateEmptyField();
     	appCreate.createPdf(UNSIGNED);
+    	appCreate.addField(SRC, UNSIGNED2);
 
 		BouncyCastleProvider provider = new BouncyCastleProvider();
 		Security.addProvider(provider);
---
+++ src/main/java/tutorial/signatures/E10_SignatureAppearances.java
@@ -0,0 +1,76 @@
+package tutorial.signatures;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.Certificate;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfSignatureAppearance.RenderingMode;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.itextpdf.text.pdf.security.MakeSignature;
+import com.itextpdf.text.pdf.security.PrivateKeySignature;
+
+public class E10_SignatureAppearances {
+
+	public static final String KEYSTORE = "src/main/resources/signatures/ks";
+	public static final String PASSWORD = "password";
+	public static final String IMG = "src/main/resources/signatures/1t3xt.gif";
+	public static final String SRC = "src/main/resources/signatures/hello_to_sign.pdf";
+	public static final String DEST = "results/signatures/signature_appearance_%s.pdf";
+	
+	public void sign(PrivateKey pk, Certificate[] chain,
+			String src, String name, String dest, String provider,
+			String reason, String location, RenderingMode renderingMode,
+			Image image, String digestAlgorithm, boolean subfilter)
+					throws GeneralSecurityException, IOException, DocumentException {
+        // Creating the reader and the stamper
+        PdfReader reader = new PdfReader(src);
+        FileOutputStream os = new FileOutputStream(dest);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        // Creating the appearance
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setReason(reason);
+        appearance.setLocation(location);
+        appearance.setVisibleSignature(name);
+        appearance.setRenderingMode(renderingMode);
+        appearance.setSignatureGraphic(image);
+        // Creating the signature
+        PrivateKeySignature pks = new PrivateKeySignature(pk, digestAlgorithm, provider);
+        MakeSignature.signDetached(appearance, pks, chain, null, null, null, provider, 0, subfilter);
+	}
+	
+	public static void main(String[] args) throws GeneralSecurityException, IOException, DocumentException {
+		BouncyCastleProvider provider = new BouncyCastleProvider();
+		Security.addProvider(provider);
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(new FileInputStream(KEYSTORE), PASSWORD.toCharArray());
+        String alias = (String)ks.aliases().nextElement();
+        PrivateKey pk = (PrivateKey) ks.getKey(alias, PASSWORD.toCharArray());
+        Certificate[] chain = ks.getCertificateChain(alias);
+        Image image = Image.getInstance(IMG);
+        E10_SignatureAppearances app = new E10_SignatureAppearances();
+        app.sign(pk, chain, SRC, "Signature1", String.format(DEST, 1), provider.getName(),
+        		"Appearance 1", "Ghent", RenderingMode.DESCRIPTION, null,
+        		DigestAlgorithms.SHA256, MakeSignature.CMS);
+        app.sign(pk, chain, SRC, "Signature1", String.format(DEST, 2), provider.getName(),
+        		"Appearance 2", "Ghent", RenderingMode.NAME_AND_DESCRIPTION, null,
+        		DigestAlgorithms.SHA256, MakeSignature.CMS);
+        app.sign(pk, chain, SRC, "Signature1", String.format(DEST, 3), provider.getName(),
+        		"Appearance 3", "Ghent", RenderingMode.GRAPHIC_AND_DESCRIPTION, image,
+        		DigestAlgorithms.SHA256, MakeSignature.CMS);
+        app.sign(pk, chain, SRC, "Signature1", String.format(DEST, 4), provider.getName(),
+        		"Appearance 4", "Ghent", RenderingMode.GRAPHIC, image,
+        		DigestAlgorithms.SHA256, MakeSignature.CMS);
+	}
+}
