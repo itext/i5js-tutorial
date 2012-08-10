d5d8ddcc33f78d9f29024aba6b406f1ccdd0c253
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-08-10 12:21:23 +0000

    New examples

---
+++ signatures/src/main/java/signatures/chapter04/C4_05_GetOcspUrl.java
@@ -0,0 +1,35 @@
+package signatures.chapter04;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.Security;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import com.itextpdf.text.pdf.security.CertificateUtil;
+
+public class C4_05_GetOcspUrl {
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("c:/home/blowagie/key.properties"));
+    	String path = properties.getProperty("PRIVATE");
+        String pass = properties.getProperty("PASSWORD");
+        
+		BouncyCastleProvider provider = new BouncyCastleProvider();
+		Security.addProvider(provider);
+        KeyStore ks = KeyStore.getInstance("pkcs12", provider.getName());
+		ks.load(new FileInputStream(path), pass.toCharArray());
+        String alias = (String)ks.aliases().nextElement();
+        Certificate[] chain = ks.getCertificateChain(alias);	    
+        for (int i = 0; i < chain.length; i++) {
+        	X509Certificate cert = (X509Certificate)chain[i];
+        	System.out.println(String.format("[%s] %s", i, cert.getSubjectDN()));
+			System.out.println(CertificateUtil.getOCSPURL(cert));
+        }
+	}
+}
---
+++ signatures/src/main/java/signatures/chapter04/C4_06_SignWithOCSP.java
@@ -0,0 +1,43 @@
+package signatures.chapter04;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.Certificate;
+import java.util.Properties;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.itextpdf.text.pdf.security.MakeSignature;
+import com.itextpdf.text.pdf.security.OcspClient;
+import com.itextpdf.text.pdf.security.OcspClientBouncyCastle;
+
+public class C4_06_SignWithOCSP extends C4_01_SignWithCAcert {
+	public static final String SRC = "src/main/resources/hello.pdf";
+	public static final String DEST = "results/hello_cacert_ocsp.pdf";
+	
+	public static void main(String[] args) throws IOException, GeneralSecurityException, DocumentException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("c:/home/blowagie/key.properties"));
+    	String path = properties.getProperty("PRIVATE");
+        String pass = properties.getProperty("PASSWORD");
+
+		BouncyCastleProvider provider = new BouncyCastleProvider();
+		Security.addProvider(provider);
+        KeyStore ks = KeyStore.getInstance("pkcs12", provider.getName());
+		ks.load(new FileInputStream(path), pass.toCharArray());
+        String alias = (String)ks.aliases().nextElement();
+        PrivateKey pk = (PrivateKey) ks.getKey(alias, pass.toCharArray());
+        Certificate[] chain = ks.getCertificateChain(alias);
+        OcspClient ocspClient = new OcspClientBouncyCastle();
+        C4_06_SignWithOCSP app = new C4_06_SignWithOCSP();
+		app.sign(pk, chain, SRC, DEST, provider.getName(), "Test", "Ghent", DigestAlgorithms.SHA256, MakeSignature.CMS,
+				null, ocspClient, null, 0);
+	}
+  
+}
