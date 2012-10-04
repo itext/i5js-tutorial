6cb223e94ee4689722266f1c21015e9312b30e66
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-10-04 17:13:52 +0000

    Removing the unused import statements.

--- signatures/src/main/java/signatures/chapter5/C5_06_ValidateLTV.java
+++ signatures/src/main/java/signatures/chapter5/C5_06_ValidateLTV.java
@@ -1,45 +1,17 @@
 package signatures.chapter5;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.Security;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509CRL;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.cert.ocsp.BasicOCSPResp;
-import org.bouncycastle.cert.ocsp.CertificateStatus;
 import org.bouncycastle.cert.ocsp.OCSPException;
-import org.bouncycastle.cert.ocsp.OCSPResp;
-import org.bouncycastle.cert.ocsp.SingleResp;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.ContentVerifierProvider;
 import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.bc.BcDigestCalculatorProvider;
-import org.bouncycastle.operator.jcajce.JcaContentVerifierProviderBuilder;
 
-import com.itextpdf.text.log.Logger;
 import com.itextpdf.text.log.LoggerFactory;
 import com.itextpdf.text.log.SysoLogger;
-import com.itextpdf.text.pdf.AcroFields;
-import com.itextpdf.text.pdf.PRStream;
-import com.itextpdf.text.pdf.PdfArray;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.security.CertificateUtil;
 import com.itextpdf.text.pdf.security.LtvValidation;
-import com.itextpdf.text.pdf.security.OcspClientBouncyCastle;
-import com.itextpdf.text.pdf.security.PdfPKCS7;
 
 public class C5_06_ValidateLTV {
 	public static final String EXAMPLE1 = "results/chapter5/ltv_1.pdf";
