a9c3383a759ba1b2dab0c5b2e59df4c901fc79d0
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-10-05 16:12:38 +0000

    Updated the example.

--- signatures/src/main/java/signatures/chapter5/C5_06_ValidateLTV.java
+++ signatures/src/main/java/signatures/chapter5/C5_06_ValidateLTV.java
@@ -1,8 +1,11 @@
 package signatures.chapter5;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.security.Security;
+import java.security.cert.CertificateFactory;
 
 import org.bouncycastle.cert.ocsp.OCSPException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -12,8 +15,10 @@ import com.itextpdf.text.log.LoggerFactory;
 import com.itextpdf.text.log.SysoLogger;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.security.LtvValidation;
+import com.itextpdf.text.pdf.security.LtvVerification.CertificateOption;
 
 public class C5_06_ValidateLTV {
+	public static final String ADOBE = "src/main/resources/adobeRootCA.cer";
 	public static final String EXAMPLE1 = "results/chapter5/ltv_1.pdf";
 	public static final String EXAMPLE2 = "results/chapter5/ltv_2.pdf";
 	public static final String EXAMPLE3 = "results/chapter5/ltv_3.pdf";
@@ -38,7 +43,16 @@ public class C5_06_ValidateLTV {
 	}
 	
 	public void validate(PdfReader reader) throws IOException, GeneralSecurityException, OCSPException, OperatorCreationException {
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(null, null);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		ks.setCertificateEntry("adobe",
+				cf.generateCertificate(new FileInputStream(ADOBE)));
+		
  		LtvValidation data = new LtvValidation(reader);
+ 		data.setCertificateOption(CertificateOption.WHOLE_CHAIN);
+ 		data.setVerifyRootCertificate(false);
+ 		data.setKeyStore(ks);
 		data.verify();
 	}
 }
