546ae70aa125eb602ea449a9efcd4b0762b70921
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-06-13 14:49:42 +0000

    Other output for example 0303

--- basics/src/main/java/basics/chapter03/C0303_PageNumbers.java
+++ basics/src/main/java/basics/chapter03/C0303_PageNumbers.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.itextpdf.text.io.RandomAccessSource;
 import com.itextpdf.text.io.RandomAccessSourceFactory;
 import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
 
 public class C0303_PageNumbers {
@@ -16,7 +17,7 @@ public class C0303_PageNumbers {
 		PdfDictionary page;
 		for (int i = 1; i <= n; i++) {
 			page = reader.getPageN(i);
-			System.out.println(page);
+			System.out.println("The parent of page " + i + " is " + page.get(PdfName.PARENT));
 		}
 	}
 }
