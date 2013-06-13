1d1c64a368b75cbdf86947f38b0ee971ad83240a
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-06-13 19:19:59 +0000

    New example

--- basics/src/main/java/basics/chapter02/C0201_HelloWorld.java
+++ basics/src/main/java/basics/chapter02/C0201_HelloWorld.java
@@ -9,11 +9,19 @@ import java.util.List;
 import com.itextpdf.text.Anchor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfDestination;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfNumber;
 import com.itextpdf.text.pdf.PdfOutline;
+import com.itextpdf.text.pdf.PdfPageLabels;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class C0201_HelloWorld {
@@ -23,6 +31,7 @@ public class C0201_HelloWorld {
 		createPdf2();
 		createPdf3();
 		createPdf4();
+		createPdf5();
 	}
 	
 	public static void createPdf1() throws IOException, DocumentException {
@@ -88,5 +97,65 @@ public class C0201_HelloWorld {
 	      }
 	    }
 	    return factors;
-	  }
+	}
+	
+	public static void createPdf5() throws IOException, DocumentException {
+		Document document = new Document();
+		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("pages.pdf"));
+		writer.setViewerPreferences(PdfWriter.PageLayoutTwoPageLeft);
+		writer.addViewerPreference(PdfName.PRINTSCALING, PdfName.NONE);
+		PdfPageLabels labels = new PdfPageLabels();
+        labels.addPageLabel(1, PdfPageLabels.UPPERCASE_LETTERS);
+        labels.addPageLabel(3, PdfPageLabels.DECIMAL_ARABIC_NUMERALS);
+        labels.addPageLabel(4,
+            PdfPageLabels.DECIMAL_ARABIC_NUMERALS, "Custom-", 2);
+		writer.setPageLabels(labels);
+		document.open();
+		document.add(new Paragraph("Hello World"));
+		document.add(new Paragraph("Hello People"));
+		document.newPage();
+
+        // we add the text to the direct content, but not in the right order
+        PdfContentByte cb = writer.getDirectContent();
+        BaseFont bf = BaseFont.createFont();
+        cb.beginText();
+        cb.setFontAndSize(bf, 12);
+        cb.moveText(88.66f, 788); 
+        cb.showText("ld");
+        cb.moveText(-22f, 0); 
+        cb.showText("Wor");
+        cb.moveText(-15.33f, 0); 
+        cb.showText("llo");
+        cb.moveText(-15.33f, 0); 
+        cb.showText("He");
+        cb.endText();
+        // we also add text in a form XObject
+        PdfTemplate tmp = cb.createTemplate(250, 25);
+        tmp.beginText();
+        tmp.setFontAndSize(bf, 12);
+        tmp.moveText(0, 7);
+        tmp.showText("Hello People");
+        tmp.endText();
+        cb.addTemplate(tmp, 36, 763);
+               
+        document.setPageSize(PageSize.A4.rotate());
+        document.newPage();
+		document.add(new Paragraph("Hello World"));
+		
+		document.setPageSize(new Rectangle(842, 595));
+		document.newPage();
+		document.add(new Paragraph("Hello World"));
+		document.setPageSize(PageSize.A4);
+		
+		writer.setCropBoxSize(new Rectangle(40, 40, 565, 795));
+		document.newPage();
+		document.add(new Paragraph("Hello World"));
+
+		writer.setCropBoxSize(null);
+		writer.addPageDictEntry(PdfName.USERUNIT, new PdfNumber(5));
+		document.newPage();
+		document.add(new Paragraph("Hello World"));
+		
+		document.close();
+	}
 }
