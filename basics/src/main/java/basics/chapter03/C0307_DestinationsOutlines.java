b1cca02bfe873d4e450bd1e18019c8f3a1abe8df
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-07-04 16:00:16 +0000

    An example had the wrong sequence number.

--- basics/src/main/java/basics/chapter03/C0307_DestinationsOutlines.java
+++ basics/src/main/java/basics/chapter03/C0307_DestinationsOutlines.java
@@ -1,15 +1,18 @@
 package basics.chapter03;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.itextpdf.text.pdf.PdfArray;
 import com.itextpdf.text.pdf.PdfDictionary;
 import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.SimpleBookmark;
 import com.itextpdf.text.pdf.SimpleNamedDestination;
 
-public class C0306_DestinationsOutlines {
+public class C0307_DestinationsOutlines {
 
 	public static void main(String[] args) throws IOException {
 		PdfReader reader = new PdfReader("src/main/resources/primes.pdf");
@@ -23,6 +26,25 @@ public class C0306_DestinationsOutlines {
 		Map<String, String> map = SimpleNamedDestination.getNamedDestination(reader, false);
 		System.out.println(map.get("Prime101"));
 		
+		PdfDictionary outlines = catalog.getAsDict(PdfName.OUTLINES);
+		System.out.println("Root:");
+		showEntries(outlines);
+		System.out.println("First:");
+		showEntries(outlines.getAsDict(PdfName.FIRST));
+		System.out.println("Last:");
+		showEntries(outlines.getAsDict(PdfName.LAST));
+		
+		List<HashMap<String, Object>> bookmarks = SimpleBookmark.getBookmark(reader);
+		for (HashMap<String, Object> item : bookmarks) {
+			System.out.println(item);
+		}		
 		reader.close();
+	}
+	
+	public static void showEntries(PdfDictionary dict) {
+		for (PdfName key : dict.getKeys()) {
+			System.out.print(key + ": ");
+			System.out.println(dict.get(key));
+		}
 	}
 }
